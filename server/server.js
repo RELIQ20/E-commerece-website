b50e8735893622570bf32120ade2575507970f97
Reliq <nathaniellawas05@gmail.com>
2025-05-07 20:28:08 +0800

    with frontend na walay sulod

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {connectDB} from './config/database.js';
 import productRoute from './routes/product.route.js'
+import { postProduct } from './controller/controller.js';
 
 dotenv.config();
 
@@ -9,10 +10,13 @@ const app = express();
 
 app.use(express.json()); // allows to accept JSON data in the req.body
 
-app.use("/api/products", productRoute);
+const PORT = process.env.PORT || 5000 //default is 5000 
+
+app.use("/api/products", productRoute);//mahimong prefix
+
 //listen and works to port 5000 and prompts 'server is running'
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log("Server is running...");
+    console.log("Server is running..." + PORT);
 });
 
